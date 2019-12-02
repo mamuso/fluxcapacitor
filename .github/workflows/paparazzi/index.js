02e9b0161bd0cf2053ea943ce1c9e5683f057c1f
Manuel Muñoz Solera <mamuso@github.com>
2019-12-01 23:41:58 -0800

    Moving action to node

--- .github/workflows/paparazzi/index.js
+++ .github/workflows/paparazzi/index.js
@@ -1,4 +1,11 @@
 const puppeteer = require('puppeteer');
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const githubtoken = core.getInput("GITHUB_TOKEN");
+const octokit = new github.GitHub(githubtoken);
+const context = github.context;
+const date = new Date().toISOString().split("T")[0];
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -9,7 +16,7 @@ const puppeteer = require('puppeteer');
   await page.setViewport({width: 1920, height: 1080});
   await page.goto('https://github.com');
   await page.screenshot({path: 'github.png', fullPage: true});
-
   await browser.close();
-  await console.log("cool!");
+  
+  await ()
 })();
