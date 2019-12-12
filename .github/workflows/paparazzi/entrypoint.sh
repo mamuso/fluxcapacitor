aa0332d5936cc650256781a683b598616a7ea7e0
Manuel Muñoz Solera <mamuso@github.com>
2019-12-12 15:01:52 -0800

    Cloning

--- .github/workflows/paparazzi/entrypoint.sh
+++ .github/workflows/paparazzi/entrypoint.sh
@@ -6,5 +6,11 @@ if [ -z "${GITHUB_TOKEN}" ]; then
     exit 1
 fi
 
-# install lfs hooks
-git lfs install
+# initialize repository
+remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git config user.name "Paparazzi"
+git config user.email "Paparazzi-action@users.noreply.github.com"
+git clone "${remote_repo}" repo
+cd repo
+git show-ref # useful for debugging
+git branch --verbose
