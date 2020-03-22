9f44622bf62f7a4de968a65641b14387ceaa30ae
Manuel Muñoz Solera <mamuso@github.com>
2020-03-21 23:08:04 -0700

    Incremental correctness

--- .github/workflows/paparazzi/entrypoint.sh
+++ .github/workflows/paparazzi/entrypoint.sh
@@ -5,18 +5,20 @@ if [ -z "${GITHUB_TOKEN}" ]; then
     exit 1
 fi
 
-# Clone repo
-remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git config --global user.name "Paparazzi"
-git config --global user.email "paparazzi-action@users.noreply.github.com"
-git clone "${remote_repo}" /github/workspace/repo
-cd repo
-git show-ref # useful for debugging
-git branch --verbose
+echo "lalalalala"
 
-# Take some pictures
-cd /
-GITHUB_ACTIONS=true node dist/paparazzi.js
+# # Clone repo
+# remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+# git config --global user.name "Paparazzi"
+# git config --global user.email "paparazzi-action@users.noreply.github.com"
+# git clone "${remote_repo}" /github/workspace/repo
+# cd repo
+# git show-ref # useful for debugging
+# git branch --verbose
+
+# # Take some pictures
+# cd /
+# GITHUB_ACTIONS=true node dist/paparazzi.js
 
 # Push files to the repo
 # cd /github/workspace/repo
