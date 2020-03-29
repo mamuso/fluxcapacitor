f83c2d47ad00b9a76a69fed436a234f2e41dec3b
Manuel Muñoz Solera <mamuso@github.com>
2020-03-28 19:24:31 -0700

    typo

--- .github/workflows/paparazzi/playground.js
+++ .github/workflows/paparazzi/playground.js
@@ -14,59 +14,60 @@ const sharedAccessPolicy = {
   }
 }
 
-const fileService = azure.createFileService();
-  (async function() {
-    const browser = await playwright.chromium.launch([])
-    const page = await browser.newPage()
-    await page.goto('https://github.com/')
-    await page.screenshot({path: `test.png`})
+const fileService = azure.createFileService()
 
-    await fileService.createShareIfNotExists('fluxshare', function(
-      error,
-      result,
-      response
-    ) {
+// ---
+(async function() {
+  const browser = await playwright.chromium.launch([])
+  const page = await browser.newPage()
+  await page.goto('https://github.com/')
+  await page.screenshot({path: `test.png`})
+
+  await fileService.createShareIfNotExists('fluxshare', function(
+    error,
+    result,
+    response
+  ) {
+    if (!error) {
+      console.log(result)
+    } else {
+      console.log(error)
+    }
+  })
+  await fileService.createDirectoryIfNotExists(
+    'fluxshare',
+    'screenshots',
+    function(error, result, response) {
       if (!error) {
         console.log(result)
       } else {
         console.log(error)
       }
-    })
-    await fileService.createDirectoryIfNotExists(
-      'fluxshare',
-      'screenshots',
-      function(error, result, response) {
-        if (!error) {
-          console.log(result)
-        } else {
-          console.log(error)
-        }
+    }
+  )
+  await fileService.createFileFromLocalFile(
+    'fluxshare',
+    'screenshots',
+    'test.png',
+    'test.png',
+    function(error, result, response) {
+      if (!error) {
+        console.log(result)
+      } else {
+        console.log(error)
       }
-    )
-    await fileService.createFileFromLocalFile(
-      'fluxshare',
-      'screenshots',
-      'test.png',
-      'test.png',
-      function(error, result, response) {
-        if (!error) {
-          console.log(result)
-        } else {
-          console.log(error)
-        }
-      }
-    )
-    const token = await fileService.generateSharedAccessSignature(
-      'fluxshare',
-      'screenshots',
-      'test.jpg',
-      sharedAccessPolicy
-    )
-    await fileService.getUrl('fluxshare', 'screenshots', 'test.png', token)
+    }
+  )
+  const token = await fileService.generateSharedAccessSignature(
+    'fluxshare',
+    'screenshots',
+    'test.jpg',
+    sharedAccessPolicy
+  )
+  await fileService.getUrl('fluxshare', 'screenshots', 'test.png', token)
 
-    await console.log(url)
-    await browser.close()
-  })()
-  .catch(e => {
-    console.error(e)
-  })
+  await console.log(url)
+  await browser.close()
+})().catch(e => {
+  console.error(e)
+})
