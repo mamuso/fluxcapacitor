b020d2cab1261a9cc853fd71d9b65c8ca6b461a3
Manuel Muñoz Solera <mamuso@github.com>
2020-04-20 22:53:02 -0700

    first nextjs functional scaffold

---
+++ web/pages/capture/[slug].js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import * as pluralize from "pluralize";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+function CapturePage({ capture }) {
+  return <div>....</div>;
+}
+
+export default CapturePage;
