09ed37940984ccfa8e5ba707195590119c0d6ce9
Manuel Muñoz Solera <mamuso@github.com>
2020-05-01 23:51:20 -0700

    fixing next-config

--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,9 @@
+const path = require("path");
+
 module.exports = {
   target: "serverless",
+  webpack: (config) => {
+    config.resolve.alias["~"] = path.resolve("./");
+    return config;
+  },
 };
